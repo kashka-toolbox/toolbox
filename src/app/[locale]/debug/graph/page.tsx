cbf5e7d925276c3202296e2848dbf0bccb5a5c72
Morten Renner <github@mortenrenner.de>
2026-08-23 14:44:11 +0200

    add: abs, ceil, floor, round & sign nodes

--- src/app/[locale]/debug/graph/page.tsx
+++ src/app/[locale]/debug/graph/page.tsx
@@ -21,7 +21,8 @@ graphStore.getState().initialize([
     createNode("concatenateStrings", "3", { x: 470, y: 85 }),
     createNode("waitAndForward", "4", { x: 690, y: 0 }),
     createNode("output", "5", { x: 880, y: 85 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result"}}),
-    createNode("inputNumeric", "6", { x: 50, y: 200 }),
+    createNode("input", "6", { x: 50, y: 150 }),
+    // Math:
     createNode("inputNumeric", "10", { x: 50, y: 300 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Number A"}}),
     createNode("inputNumeric", "11", { x: 50, y: 400 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Number B"}}),
     createNode("addition", "15", { x: 400, y: 300 }),
@@ -32,6 +33,21 @@ graphStore.getState().initialize([
     createNode("output", "27", { x: 680, y: 500 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Multiplication"}}),
     createNode("division", "18", { x: 400, y: 750 }),
     createNode("output", "28", { x: 680, y: 600 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Division"}}),
+    // Math: Absolute Value
+    createNode("abs", "19", { x: 400, y: 900 }),
+    createNode("output", "19o", { x: 680, y: 700 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Abs"}}),
+    // Math: Ceil
+    createNode("ceil", "20", { x: 400, y: 1050 }),
+    createNode("output", "20o", { x: 680, y: 800 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Ceil"}}),
+    // Math: Floor
+    createNode("floor", "21", { x: 400, y: 1200 }),
+    createNode("output", "21o", { x: 680, y: 900 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Floor"}}),
+    // Math: Round
+    createNode("round", "22", { x: 400, y: 1350 }),
+    createNode("output", "22o", { x: 680, y: 1000 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Round"}}),
+    // Math: Sign
+    createNode("sign", "23", { x: 400, y: 1500 }),
+    createNode("output", "23o", { x: 680, y: 1100 }, {[NODE_SETTING_UI_LABEL_TEXT]: {value: "Result Sign"}}),
     
 ], [{
     fromIO: { nodeId: "1", nodeIOName: NODE_INPUT_IO_NAME },
@@ -90,6 +106,36 @@ graphStore.getState().initialize([
 }, {
     fromIO: { nodeId: "18", nodeIOName: "result" },
     toIO: { nodeId: "28", nodeIOName: NODE_OUTPUT_IO_NAME },
+}, { //  --- Math: Abs ---
+    fromIO: { nodeId: "10", nodeIOName: NODE_INPUT_IO_NAME },
+    toIO: { nodeId: "19", nodeIOName: "number" },
+}, {
+    fromIO: { nodeId: "19", nodeIOName: "result" },
+    toIO: { nodeId: "19o", nodeIOName: NODE_OUTPUT_IO_NAME },
+}, { //  --- Math: Ceil ---
+    fromIO: { nodeId: "10", nodeIOName: NODE_INPUT_IO_NAME },
+    toIO: { nodeId: "20", nodeIOName: "number" },
+}, {
+    fromIO: { nodeId: "20", nodeIOName: "result" },
+    toIO: { nodeId: "20o", nodeIOName: NODE_OUTPUT_IO_NAME },
+}, { //  --- Math: Floor ---
+    fromIO: { nodeId: "10", nodeIOName: NODE_INPUT_IO_NAME },
+    toIO: { nodeId: "21", nodeIOName: "number" },
+}, {
+    fromIO: { nodeId: "21", nodeIOName: "result" },
+    toIO: { nodeId: "21o", nodeIOName: NODE_OUTPUT_IO_NAME },
+}, { //  --- Math: Round ---
+    fromIO: { nodeId: "10", nodeIOName: NODE_INPUT_IO_NAME },
+    toIO: { nodeId: "22", nodeIOName: "number" },
+}, {
+    fromIO: { nodeId: "22", nodeIOName: "result" },
+    toIO: { nodeId: "22o", nodeIOName: NODE_OUTPUT_IO_NAME },
+}, { //  --- Math: Sign ---
+    fromIO: { nodeId: "10", nodeIOName: NODE_INPUT_IO_NAME },
+    toIO: { nodeId: "23", nodeIOName: "number" },
+}, {
+    fromIO: { nodeId: "23", nodeIOName: "result" },
+    toIO: { nodeId: "23o", nodeIOName: NODE_OUTPUT_IO_NAME },
 }
 
 
--- src/lib/graph/NodeDefinitions/NodeDefinitionsMath.ts
+++ src/lib/graph/NodeDefinitions/NodeDefinitionsMath.ts
@@ -9,7 +9,12 @@ export type NODE_TYPE_MATH =
     | "addition"
     | "substraction"
     | "multiplication"
-    | "division";
+    | "division"
+    | "abs"
+    | "ceil"
+    | "floor"
+    | "round"
+    | "sign";
 
 export const NODE_DEFINITIONS_MATH: {
     [key in NODE_TYPE_MATH]: NodeDefinition<any, any, any>;
@@ -187,5 +192,174 @@ export const NODE_DEFINITIONS_MATH: {
         nodeCategory: "math",
         settings: {}
     }),
-
+abs: createNodeDefinition<
+    [
+        "number",
+    ],
+    ["result"],
+    []
+>({
+    type: "operation",
+    name: "operation.math.abs",
+    inputs: [{
+        name: "number",
+        translationKey: "nodes.operation.math.abs.inputs.number",
+        type: "numeric.any",
+    }],
+    outputs: [{
+        name: "result",
+        translationKey: "nodes.operation.math.abs.outputs.result",
+        type: "numeric.any",
+    }],
+    execute: async (
+        parameters: {
+            "number": number;
+        },
+    ) => {
+        const input =
+            parameters["number"];
+        return {
+            "result":
+                Math.abs(input),
+        };
+    },
+    nodeCategory: "math",
+    settings: {}
+}),
+ceil: createNodeDefinition<
+    [
+        "number",
+    ],
+    ["result"],
+    []
+>({
+    type: "operation",
+    name: "operation.math.ceil",
+    inputs: [{
+        name: "number",
+        translationKey: "nodes.operation.math.ceil.inputs.number",
+        type: "numeric.any",
+    }],
+    outputs: [{
+        name: "result",
+        translationKey: "nodes.operation.math.ceil.outputs.result",
+        type: "numeric.any",
+    }],
+    execute: async (
+        parameters: {
+            "number": number;
+        },
+    ) => {
+        const input =
+            parameters["number"];
+        return {
+            "result":
+                Math.ceil(input),
+        };
+    },
+    nodeCategory: "math",
+    settings: {}
+}),
+floor: createNodeDefinition<
+    [
+        "number",
+    ],
+    ["result"],
+    []
+>({
+    type: "operation",
+    name: "operation.math.floor",
+    inputs: [{
+        name: "number",
+        translationKey: "nodes.operation.math.floor.inputs.number",
+        type: "numeric.any",
+    }],
+    outputs: [{
+        name: "result",
+        translationKey: "nodes.operation.math.floor.outputs.result",
+        type: "numeric.any",
+    }],
+    execute: async (
+        parameters: {
+            "number": number;
+        },
+    ) => {
+        const input =
+            parameters["number"];
+        return {
+            "result":
+                Math.floor(input),
+        };
+    },
+    nodeCategory: "math",
+    settings: {}
+}),
+round: createNodeDefinition<
+    [
+        "number",
+    ],
+    ["result"],
+    []
+>({
+    type: "operation",
+    name: "operation.math.round",
+    inputs: [{
+        name: "number",
+        translationKey: "nodes.operation.math.round.inputs.number",
+        type: "numeric.any",
+    }],
+    outputs: [{
+        name: "result",
+        translationKey: "nodes.operation.math.round.outputs.result",
+        type: "numeric.any",
+    }],
+    execute: async (
+        parameters: {
+            "number": number;
+        },
+    ) => {
+        const input =
+            parameters["number"];
+        return {
+            "result":
+                Math.round(input),
+        };
+    },
+    nodeCategory: "math",
+    settings: {}
+}),
+sign: createNodeDefinition<
+    [
+        "number",
+    ],
+    ["result"],
+    []
+>({
+    type: "operation",
+    name: "operation.math.sign",
+    inputs: [{
+        name: "number",
+        translationKey: "nodes.operation.math.sign.inputs.number",
+        type: "numeric.any",
+    }],
+    outputs: [{
+        name: "result",
+        translationKey: "nodes.operation.math.sign.outputs.result",
+        type: "numeric.any",
+    }],
+    execute: async (
+        parameters: {
+            "number": number;
+        },
+    ) => {
+        const input =
+            parameters["number"];
+        return {
+            "result":
+                Math.sign(input),
+        };
+    },
+    nodeCategory: "math",
+    settings: {}
+}),
 };  
